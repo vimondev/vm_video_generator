673b1cc884417e4d73e477fcf85c1deb71d220aa
LUS <jamborl11@gmail.com>
2021-12-24 17:57:19 +0900

    [LUS] 1224 Add ml_kit_helper

--- lib/test_mlkit.dart
+++ lib/test_mlkit.dart
@@ -1,111 +1,16 @@
-import 'package:file_saver/file_saver.dart';
-import 'package:flutter_archive/flutter_archive.dart';
-import 'package:myapp/vm_sdk/impl/ffmpeg_manager.dart';
 import 'package:path/path.dart';
-
 import 'package:flutter_ffmpeg/flutter_ffmpeg.dart';
-import 'package:path_provider/path_provider.dart';
 
 import 'vm_sdk/types/types.dart';
 import 'vm_sdk/impl/global_helper.dart';
+import 'vm_sdk/impl/ml_kit_helper.dart';
 import 'dart:convert';
-import 'dart:io';
 import 'package:flutter/services.dart' show rootBundle;
-import 'package:google_ml_kit/google_ml_kit.dart';
 
-final FaceDetector faceDetector = GoogleMlKit.vision.faceDetector();
-final ImageLabeler imageLabeler = GoogleMlKit.vision.imageLabeler();
-FFMpegManager ffMpegManager = FFMpegManager();
 FlutterFFprobe ffprobe = FlutterFFprobe();
 
-class TestMedia {
-  String filename;
-  EMediaType type;
-  int width;
-  int height;
-  List<String> frames;
-
-  TestMedia(this.filename, this.type, this.width, this.height, this.frames);
-}
-
-Map convertFaceToMap(Face face) {
-  final Map map = {};
-
-  map["boundingBox"] = <String, dynamic>{
-    'top': face.boundingBox.top,
-    'bottom': face.boundingBox.bottom,
-    'left': face.boundingBox.left,
-    'right': face.boundingBox.right
-  };
-  map["headEulerAngleY"] = face.headEulerAngleY;
-  map["headEulerAngleZ"] = face.headEulerAngleZ;
-  map["leftEyeOpenProbability"] = face.leftEyeOpenProbability;
-  map["rightEyeOpenProbability"] = face.rightEyeOpenProbability;
-  map["smilingProbability"] = face.smilingProbability;
-  map["trackingId"] = face.trackingId;
-
-  return map;
-}
-
-Map convertImageLabelToMap(ImageLabel label) {
-  final Map map = {};
-
-  map["confidence"] = label.confidence;
-  map["index"] = label.index;
-  map["label"] = label.label;
-
-  return map;
-}
-
-Future<void> sleep() async {
-  return Future.delayed(const Duration(milliseconds: 100));
-}
-
-int currentDetectCount = 0;
-Future<Map> detectObjects(String path) async {
-  while (currentDetectCount >= 100) {
-    await sleep();
-  }
-  currentDetectCount++;
-
-  List<Map> faceList = [];
-  List<Map> labelList = [];
-  try {
-    final InputImage inputImage = InputImage.fromFilePath(path);
-
-    final detectedFaceList = await faceDetector.processImage(inputImage);
-    for (final face in detectedFaceList) {
-      faceList.add(convertFaceToMap(face));
-    }
-
-    final detectedLabelList = await imageLabeler.processImage(inputImage);
-    for (final label in detectedLabelList) {
-      labelList.add(convertImageLabelToMap(label));
-    }
-  } catch (e) {}
-
-  currentDetectCount--;
-  return {'faceList': faceList, 'labelList': labelList};
-}
-
-Future<Map> runDetect(TestMedia media) async {
-  final Map map = {
-    'filename': basename(media.filename),
-    'width': media.width,
-    'height': media.height
-  };
-
-  List<Future<Map>> futures = [];
-  for (final frame in media.frames) {
-    futures.add(detectObjects(frame));
-  }
-  map["detected"] = await Future.wait(futures);
-
-  return map;
-}
-
 void testMethod() async {
-  const String testAssetPath = "_mlkittest/set01";
+  const String testAssetPath = "_test/set02";
   final filelist = [];
 
   final manifestContent = await rootBundle.loadString('AssetManifest.json');
@@ -117,21 +22,10 @@ void testMethod() async {
     }
   }
 
-  final String baseFolder = await getAppDirectoryPath();
-  final String resultFolder = "$baseFolder/results";
-  final dir = Directory(resultFolder);
-  if (await dir.exists()) {
-    await dir.delete(recursive: true);
-  }
-  await dir.create(recursive: true);
-
-  List<File> fileObjList = <File>[];
-
-  List<TestMedia> testMediaList = [];
+  List<MediaData> testMediaList = [];
   for (final String filename in filelist) {
     final writedFile =
         await copyAssetToLocalDirectory("$testAssetPath/$filename");
-    fileObjList.add(writedFile);
 
     final mediaInfo = await ffprobe.getMediaInformation(writedFile.path);
     final streams = mediaInfo.getStreams()![0].getAllProperties();
@@ -139,49 +33,12 @@ void testMethod() async {
     int width = streams["width"];
     int height = streams["height"];
     EMediaType type = EMediaType.image;
-    List<String> frames = [];
-
-    int scaledWidth = -1;
-    int scaledHeight = -1;
-
-    if (width > height) {
-      scaledWidth = 480;
-      scaledHeight = (height * (scaledWidth / width)).floor();
-      if (scaledHeight % 2 == 1) scaledHeight += 1;
-    } else {
-      scaledHeight = 480;
-      scaledWidth = (width * (scaledHeight / height)).floor();
-      if (scaledWidth % 2 == 1) scaledWidth += 1;
-    }
 
     final extname = extension(filename);
-    final index = filename.indexOf(extname);
-
-    final folderName = "$resultFolder/${filename.substring(0, index)}";
-    final dir = Directory(folderName);
-    if (await dir.exists()) {
-      await dir.delete(recursive: true);
-    }
-    await dir.create(recursive: true);
 
     switch (extname.toLowerCase()) {
       case ".mp4":
       case ".mov":
-        {
-          if (await ffMpegManager.execute([
-            "-i",
-            writedFile.path,
-            "-filter_complex",
-            "fps=4,scale=$scaledWidth:$scaledHeight,setdar=dar=${scaledWidth / scaledHeight}",
-            "$folderName/%d.jpg",
-            "-y"
-          ], (p0) => null)) {
-            frames = [];
-            await for (final entity in dir.list()) {
-              frames.add(entity.path);
-            }
-          }
-        }
         type = EMediaType.video;
         break;
 
@@ -189,42 +46,24 @@ void testMethod() async {
       case ".jpeg":
       case ".png":
       default:
-        if (await ffMpegManager.execute([
-          "-i",
-          writedFile.path,
-          "-filter_complex",
-          "scale=$scaledWidth:$scaledHeight,setdar=dar=${scaledWidth / scaledHeight}",
-          "$folderName/1.jpg",
-          "-y"
-        ], (p0) => null)) {
-          frames = [];
-          await for (final entity in dir.list()) {
-            frames.add(entity.path);
-          }
-        }
         break;
     }
     testMediaList
-        .add(TestMedia(filename, type, scaledWidth, scaledHeight, frames));
+        .add(MediaData(writedFile.path, type, width, height, null, null, null));
   }
 
-  DateTime now = DateTime.now();
-  List<Future<Map>> futures = <Future<Map>>[];
-
+  List<double> durationList = [];
+  List<double> lengthList = [];
+  List<String?> results = [];
   for (final media in testMediaList) {
-    futures.add(runDetect(media));
+    DateTime now = DateTime.now();
+    final result = await extractMLKitData(media);
+    durationList.add(DateTime.now().difference(now).inMilliseconds / 1000);
+    results.add(result);
+
+    if (result != null) {
+      lengthList.add(result.length / 1024);
+    }
   }
-  List<Map> results = await Future.wait(futures);
-  await File("$resultFolder/result.json").writeAsString(json.encode(results));
-
-  print(DateTime.now().difference(now).inMilliseconds);
-
-  // final zipFile = File("$baseFolder/result.zip");
-  // await ZipFile.createFromDirectory(
-  //     sourceDir: dir, zipFile: zipFile, recurseSubDirs: true);
-
-  // final fileSaveResult = await FileSaver.instance
-  //     .saveFile("subin", await zipFile.readAsBytes(), "zip");
-
-  // print(fileSaveResult);
+  print("");
 }
---
+++ lib/vm_sdk/impl/ml_kit_helper.dart
@@ -0,0 +1,131 @@
+import 'dart:io';
+import 'dart:convert';
+import 'package:path/path.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
+
+import '../types/global.dart';
+import 'ffmpeg_manager.dart';
+import 'global_helper.dart';
+
+final FaceDetector faceDetector = GoogleMlKit.vision.faceDetector();
+final ImageLabeler imageLabeler = GoogleMlKit.vision.imageLabeler();
+final FFMpegManager ffMpegManager = FFMpegManager();
+const int convertFrame = 4;
+
+Map convertFaceToMap(Face face) {
+  final Map map = {};
+
+  map["b"] = <String, dynamic>{
+    "t": face.boundingBox.top.floor(),
+    "b": face.boundingBox.bottom.floor(),
+    "l": face.boundingBox.left.floor(),
+    "r": face.boundingBox.right.floor()
+  };
+  if (face.headEulerAngleY != null) {
+    map["ay"] = double.parse(face.headEulerAngleY!.toStringAsFixed(4));
+  }
+  if (face.headEulerAngleZ != null) {
+    map["az"] = double.parse(face.headEulerAngleZ!.toStringAsFixed(4));
+  }
+  if (face.leftEyeOpenProbability != null) {
+    map["lp"] = double.parse(face.leftEyeOpenProbability!.toStringAsFixed(4));
+  }
+  if (face.rightEyeOpenProbability != null) {
+    map["rp"] = double.parse(face.rightEyeOpenProbability!.toStringAsFixed(4));
+  }
+  if (face.smilingProbability != null) {
+    map["sp"] = double.parse(face.smilingProbability!.toStringAsFixed(4));
+  }
+  if (face.trackingId != null) {
+    map["tid"] = int.parse(face.trackingId!.toString());
+  }
+
+  return map;
+}
+
+Map convertImageLabelToMap(ImageLabel label) {
+  final Map map = {};
+
+  map["c"] = double.parse(label.confidence.toStringAsFixed(4));
+  map["id"] = label.index;
+
+  return map;
+}
+
+Future<Map> detectObjects(String path) async {
+  List<Map> faceList = [];
+  List<Map> labelList = [];
+  try {
+    final InputImage inputImage = InputImage.fromFilePath(path);
+
+    final detectedFaceList = await faceDetector.processImage(inputImage);
+    for (final face in detectedFaceList) {
+      faceList.add(convertFaceToMap(face));
+    }
+
+    final detectedLabelList = await imageLabeler.processImage(inputImage);
+    for (final label in detectedLabelList) {
+      labelList.add(convertImageLabelToMap(label));
+    }
+  } catch (e) {}
+
+  return {"f": faceList, "lb": labelList};
+}
+
+Future<List<Map>> runDetect(List<String> frames) async {
+  List<Future<Map>> futures = [];
+  for (final frame in frames) {
+    futures.add(detectObjects(frame));
+  }
+
+  return await Future.wait(futures);
+}
+
+Future<String?> extractMLKitData(MediaData data) async {
+  String? result;
+
+  final String filename = basename(data.absolutePath);
+  final String extname = extension(filename);
+  final int index = filename.indexOf(extname);
+
+  final String mlkitResultDir =
+      "${await getAppDirectoryPath()}/mlkit/${filename.substring(0, index)}";
+  final Directory dir = Directory(mlkitResultDir);
+
+  if (await dir.exists()) {
+    await dir.delete(recursive: true);
+  }
+  await dir.create(recursive: true);
+
+  int scaledWidth = -1;
+  int scaledHeight = -1;
+
+  if (data.width > data.height) {
+    scaledWidth = 480;
+    scaledHeight = (data.height * (scaledWidth / data.width)).floor();
+    if (scaledHeight % 2 == 1) scaledHeight += 1;
+  } else {
+    scaledHeight = 480;
+    scaledWidth = (data.width * (scaledHeight / data.height)).floor();
+    if (scaledWidth % 2 == 1) scaledWidth += 1;
+  }
+
+  final List<String> frames = [];
+  if (await ffMpegManager.execute([
+    "-i",
+    data.absolutePath,
+    "-filter_complex",
+    "${data.type == EMediaType.video ? "fps=$convertFrame," : ""}scale=$scaledWidth:$scaledHeight,setdar=dar=${scaledWidth / scaledHeight}",
+    "$mlkitResultDir/${data.type == EMediaType.video ? "%d" : "1"}.jpg",
+    "-y"
+  ], (p0) => null)) {
+    await for (final entity in dir.list()) {
+      frames.add(entity.path);
+    }
+  }
+
+  result = json.encode({"fps": convertFrame, "r": await runDetect(frames)});
+  await dir.delete(recursive: true);
+
+  return result;
+}
