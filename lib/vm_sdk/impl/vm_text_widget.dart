21032d5c7eed6855ddb0670c03f88cc34a4f26b0
LUS <jamborl11@gmail.com>
2024-01-10 17:43:49 +0900

    [이우섭] 0110 VMText, Rectangle 제거

--- lib/vm_sdk/impl/vm_text_widget.dart
+++ lib/vm_sdk/impl/vm_text_widget.dart
@@ -7,40 +7,8 @@ import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:myapp/vm_sdk/impl/text_helper.dart';
 import 'package:myapp/vm_sdk/types/types.dart';
 import 'package:myapp/vm_sdk/impl/global_helper.dart';
-import 'package:myapp/vm_sdk/widgets/customwebview.dart';
 import 'package:rxdart/rxdart.dart';
-import 'package:rxdart/subjects.dart';
-
-class Rectangle {
-  double _x, _y, _width, _height;
-
-  Rectangle(this._x, this._y, this._width, this._height);
-
-  double get x => _x;
-
-  double get y => _y;
-
-  double get width => _width;
-
-  double get height => _height;
-}
-
-class VMText {
-  String _key;
-  String _value;
-  Rectangle _boundingBox;
-
-  VMText(this._key, this._value, this._boundingBox);
-
-  String get key => _key;
-
-  String get value => _value;
-
-  Rectangle get boundingBox => _boundingBox;
-}
-
 class VMTextWidget extends StatelessWidget {
-
   InAppWebViewController? _controller;
   String? _currentDirPath;
   String? _currentPreviewPath;
